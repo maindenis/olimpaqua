4c3a76b0aa26e898efc98f4499b495ef702da248
maindenis <165322876+maindenis@users.noreply.github.com>
2025-01-10 21:30:00 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,24 @@ e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
-$(window).resize(function() {
+function getTriangleParams() {
+    $(".thumb").each(function() {
+        tr = $(this).find(".triangle");
+        heigth = $(this).height()/2;
+        if($(this).index() == 0) {
+            tr.css({
+                "border-width": heigth+"px 0 "+heigth+"px 95px"
+            });
+        } else {
+            tr.css({
+                "border-width": heigth+"px 95px "+heigth+"px 0"
+            });
+        }
+    });
+}
 
+$(window).resize(function() {
+    getTriangleParams();
 });
 
 $(document).scroll(function() {
@@ -13,6 +29,7 @@ $(document).scroll(function() {
 });
 
 $(document).ready(function() {
+    getTriangleParams();
     // counter = 0;
     // $(".thumbnails .thumb").each(function() {
     //     counter++;
@@ -22,38 +39,19 @@ $(document).ready(function() {
     //     }
     // });
 
-    // if( $(".slider").length > 0 ) {
-    //     $(".slider").not(".slick-initialized").slick({
-    //         dots: true,
-    //         arrows: true,
-    //         // autoplay: true,
-    //         autoplaySpeed: 4000,
-    //         speed: 2000,
-    //         variableWidth: true,
-    //         slidesToShow: 5,
-    //         slidesToScroll: 1,
-    //         centerMode: true,
-    //         appendDots: $(".slider_dots"),
-    //         appendArrows: $(".slider_arrows"),
-    //         // fade: true,
-    //         responsive: [
-    //             {
-    //               breakpoint: 1125,
-    //               settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 1
-    //               }
-    //             },
-    //             // {
-    //             //   breakpoint: 540,
-    //             //   settings: {
-    //             //     slidesToShow: 1,
-    //             //     slidesToScroll: 1
-    //             //   }
-    //             // }
-    //           ]
-    //     });
-    // }
+    if( $(".slider").length > 0 ) {
+        $(".slider").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 2000,
+            variableWidth: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: true
+        });
+    }
 
     // --------------
 
@@ -77,4 +75,53 @@ $(document).ready(function() {
         }
     });
 
+    // ----------------
+
+    function prettify(num) {
+        var n = num.toString();
+        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+    }
+
+    function my_prettify (n) {
+        var num = Math.log2(n);
+        var resPrice = prettify(n);
+        $("#rangeVal").val(resPrice + " ₽");
+        return n+" m";
+    }
+
+    $("#range").ionRangeSlider({
+        min: 1,
+        max: 120,
+        from: 0,
+        prettify: my_prettify
+    });
+
+    $(".calculator__item").on("click", function(e) {
+        e.preventDefault();
+        parent = $(this).closest(".calculator__items");
+        parent.find(".calculator__item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    // ----------------
+
+    var mapZoom;
+    var lat;
+    var long;
+    ymaps.ready(function () {     
+        mapZoom = $("#map").attr("data-zoom");
+        lat = $("#map").attr("data-lat");
+        long = $("#map").attr("data-long");   
+        var myMap = new ymaps.Map('map', {
+            center: [long, lat],
+            zoom: mapZoom
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+        myPlacemark1 = new ymaps.Placemark([long, lat], {
+            hintContent: ''
+        }, {
+        });
+        myMap.geoObjects.add(myPlacemark1);        
+    });
 });
